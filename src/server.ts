6e04c61061b135195f7006b98eebd5b7095401ab
mahmoud <mamoidtamer300@gmail.com>
2026-03-25 04:14:55 +0200

    add docker setup

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,14 @@ const dbUrl = process.env["DB_URL"];
 if (!dbUrl) {
     throw new Error("DB_URL is not defined");
 }
-
+const PORT = process.env["PORT"] || 5000;
 mongoose.connect(dbUrl)
     .then(() => {
         console.log("DB Connected");
-        app.listen(4000, "0.0.0.0", () => {
-            console.log("Server running");
+
+
+        app.listen(PORT, () => {
+            console.log(`Server running on ${PORT}`);
         });
     })
     .catch((err: any) => console.log(err));
