bfac75208548ba51cfccb9987a4f29864721a6c1
mahmoud <mamoidtamer300@gmail.com>
2026-03-27 01:14:23 +0200

    dada

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken"
 
-export const generateToken = (name: string, id: string, role: string) => {
+export const generateToken = (name: string, id: string, role: string, sessionId: string) => {
 
     const jwtSecretKey = process.env["JWT_SECRET"]
 
     if (!jwtSecretKey) throw new Error("jwtSecretKey is not defined");
-    const token = jwt.sign({ name, id, role }, jwtSecretKey, { expiresIn: "7d" })
+    const token = jwt.sign({ name, id, role, sessionId }, jwtSecretKey, { expiresIn: "7d" })
 
     return token
 }
