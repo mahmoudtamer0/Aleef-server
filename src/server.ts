8f2ab554239329d9d54d7ef18c2c38c8f88f0a07
mahmoud <mamoidtamer300@gmail.com>
2026-03-28 05:12:30 +0200

    ..

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const dbUrl = process.env["DB_URL"];
 if (!dbUrl) {
     throw new Error("DB_URL is not defined");
 }
-const PORT = process.env["PORT"] || 5000;
+const PORT = process.env["PORT"] || 3000;
 mongoose.connect(dbUrl)
     .then(() => {
         console.log("DB Connected");
