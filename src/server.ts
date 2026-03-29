7cb22850ba87070b8f933b5747bf83a2df1cf675
mahmoud <mamoidtamer300@gmail.com>
2026-03-29 05:34:36 +0200

    vercel configiration

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,14 @@ if (!dbUrl) throw new Error("DB_URL is not defined");
 
 const PORT = process.env["PORT"] || 3000;
 
-mongoose.connect(dbUrl)
-    .then(() => {
-        console.log("✅ DB Connected");
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running on port ${PORT}`);
-        });
-    })
-    .catch((err) => console.log(err));
+if (process.env["NODE_ENV"] == "development") {
+
+    mongoose.connect(dbUrl)
+        .then(() => {
+            console.log("✅ DB Connected");
+            app.listen(PORT, () => {
+                console.log(`🚀 Server running on port ${PORT}`);
+            });
+        })
+        .catch((err) => console.log(err));
+}
