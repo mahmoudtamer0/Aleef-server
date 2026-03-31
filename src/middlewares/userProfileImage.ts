88f775ea364ac39fca62c03a699ee10ae69a96ce
mahmoud <mamoidtamer300@gmail.com>
2026-03-31 04:15:44 +0200

    da

--- src/middlewares/userProfileImage.ts
+++ src/middlewares/userProfileImage.ts
@@ -19,9 +19,7 @@ export const upload = multer({
     storage: storage,
 
     fileFilter: (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
-        if (!file.mimetype.startsWith("image/")) {
-            return cb(new Error("Only images are allowed"));
-        }
+
         cb(null, true);
     }
 });
