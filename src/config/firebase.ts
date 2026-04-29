61a9b0e790fcd796ae46ed7c9ba3ffed83e3e126
mahmoud <mamoidtamer300@gmail.com>
2026-04-30 00:20:18 +0300

    stopping firebase temp

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,8 @@
-import admin from "firebase-admin";
-import serviceAccount from "./aleef-4de73-firebase-adminsdk-fbsvc-9aa06e957e.json";
+// import admin from "firebase-admin";
+// import serviceAccount from "./aleef-4de73-firebase-adminsdk-fbsvc-9aa06e957e.json";
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as any),
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount as any),
+// });
 
-export default admin;
+// export default admin;
