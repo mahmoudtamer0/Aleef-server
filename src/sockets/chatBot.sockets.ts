7fa3e7b23152da649871f2d977d74e1fc6691f57
mahmoud <mamoidtamer300@gmail.com>
2026-04-30 05:34:22 +0300

    intigrate chatbot in system with open ai

--- src/sockets/chatBot.sockets.ts
+++ src/sockets/chatBot.sockets.ts
@@ -19,7 +19,7 @@ export = (io: any, socket: any) => {
                 return;
             }
 
-            const res = await fetch("https://chatbot-production-a866.up.railway.app/chat", {
+            const botresponse = fetch("https://chatbot-production-a866.up.railway.app/chat", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -61,21 +61,9 @@ export = (io: any, socket: any) => {
                 message: data.message,
             });
 
-
-            // if (data.message.trim().length < 1) {
-            //     io.to(socket.user.id).emit("error_message", {
-            //         errMessage: "you can't send empty message :)"
-            //     });
-            //     return;
-            // }
-
-
-
-
-
-
+            const res = await botresponse;
             const dataTofetch: any = await res.json();
-            console.log("dataTofetch:", dataTofetch)
+
             io.to(socket.user.id).emit("chat_response", {
                 message: dataTofetch?.Response
             });
@@ -95,91 +83,5 @@ export = (io: any, socket: any) => {
         }
     });
 
-    // socket.on("send_message", async (data: { chatId: string; message: string }) => {
-    //     try {
-    //         const model = socket.user.role === "DOCTOR" ? "Doctor" : "User";
-
-    //         if (data.message.trim().length < 1) {
-    //             io.to(socket.user.id).emit("error_message", {
-    //                 errMessage: "you can't send empty message :)"
-    //             });
-    //             return;
-    //         }
-
-    //         const message = await createMessage({
-    //             chatId: data.chatId,
-    //             sender: socket.user.id,
-    //             senderModel: model,
-    //             message: data.message,
-    //         });
-
-
-    //         const populatedMessage = await message.populate({
-    //             path: "sender",
-    //             select: "name profilePic"
-    //         });
-    //         const sender = populatedMessage.sender as any;
-
-    //         io.to(data.chatId).emit("receive_message", {
-    //             messageId: message._id,
-    //             _id: socket.user.id,
-    //             text: populatedMessage.text,
-    //             senderId: sender._id,
-    //             senderProfilePic: sender.profilePic,
-    //             chatId: data.chatId
-    //         });
-
-    //         const chat = await Chat.findById(data.chatId);
-
-    //         if (!chat) return;
-
-    //         const otherUser = chat.members.find(
-    //             (m: any) => m.memberId.toString() !== socket.user.id
-    //         );
-
-    //         if (!otherUser) return;
-
-    //         await UnreadMessage.updateOne(
-    //             {
-    //                 userId: otherUser.memberId,
-    //                 chatId: data.chatId
-    //             },
-    //             {
-    //                 $inc: { unreadCount: 1 },
-    //                 $set: { lastMessage: populatedMessage.text }
-    //             },
-    //             { upsert: true }
-    //         );
-
-    //         chat.lastMessage = populatedMessage._id;
-    //         await chat.save();
-
-
-    //         io.to(otherUser.memberId.toString()).emit("chat_updated", {
-    //             chatId: data.chatId,
-    //             lastMessage: {
-    //                 _id: populatedMessage._id,
-    //                 text: populatedMessage.text,
-    //                 createdAt: populatedMessage.createdAt,
-    //                 sender: populatedMessage.sender
-    //             },
-    //             unreadIncrement: 1
-    //         });
-
-    //         io.to(socket.user.id.toString()).emit("chat_updated", {
-    //             chatId: data.chatId,
-    //             lastMessage: {
-    //                 _id: populatedMessage._id,
-    //                 text: populatedMessage.text,
-    //                 createdAt: populatedMessage.createdAt,
-    //                 sender: populatedMessage.sender
-    //             },
-    //             unreadIncrement: 0
-    //         });
-
-    //     } catch (err) {
-    //         console.error("Error sending message:", err);
-    //     }
-    // })
 
 };
