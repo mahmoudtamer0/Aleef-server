df8a368f3813223838ca7163bd3c1325a4fe2b1a
mahmoud <mamoidtamer300@gmail.com>
2026-05-05 02:32:23 +0300

    .......

--- src/modules/Appointments/appointment.validation.ts
+++ src/modules/Appointments/appointment.validation.ts
@@ -25,7 +25,7 @@ export const addAppointmentSchema = Joi.object({
 
     reason: Joi.string().min(3).max(500).required(),
 
-    notes: Joi.string().max(1000).optional(),
+    notes: Joi.string().max(1000).min(0).optional(),
 
 })
     .custom((value, helpers) => {
