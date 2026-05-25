9736f781e4445ed5a9178d2776a1c2f45e1e6db4
mahmoud <mamoidtamer300@gmail.com>
2026-05-26 00:55:08 +0300

    change appoinment status as admin

--- src/modules/Pet/pet.services.ts
+++ src/modules/Pet/pet.services.ts
@@ -49,7 +49,7 @@ export const addPet = async (user: any, { name, type, birthDate, gender, weight 
 
 export const getUserPets = async (userId: any) => {
 
-    const pets = await Pet.find({ owner: userId }).lean().select("name type gender profilePic birthDate weight")
+    const pets = (await Pet.find({ owner: userId }).sort({ updatedAt: -1 }).lean().select("name type gender profilePic birthDate weight updatedAt"));
     const realPets = []
     for (let i = 0; i < pets.length; i++) {
         const pet = pets[i]
