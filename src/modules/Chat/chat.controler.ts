103526bc9f71bc00295b0009a5f73a1d9af9c644
mahmoud <mamoidtamer300@gmail.com>
2026-06-10 05:46:03 +0300

    doctor performance

--- src/modules/Chat/chat.controler.ts
+++ src/modules/Chat/chat.controler.ts
@@ -24,7 +24,7 @@ export const getChatMessages = catchAsync(async (req, res, next) => {
         status: "success",
         messages: messages.messages,
         chatId: messages.chatId,
-        person: messages.person,
+        user: messages.person,
     })
 });
 
