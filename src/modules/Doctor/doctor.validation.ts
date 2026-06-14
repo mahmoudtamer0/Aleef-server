3c84d3029a2d5a27efb52c52c7a1eab87081a99a
mahmoud <mamoidtamer300@gmail.com>
2026-06-14 05:24:45 +0300

    solving doctor schedual issue

--- src/modules/Doctor/doctor.validation.ts
+++ src/modules/Doctor/doctor.validation.ts
@@ -59,10 +59,10 @@ export const editScheduleSchema = Joi.object({
                 .valid('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday')
                 .required(),
             start_time: Joi.string()
-                .pattern(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)
+                .pattern(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/)
                 .when('is_available', { is: true, then: Joi.required() }),
             end_time: Joi.string()
-                .pattern(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/)
+                .pattern(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/)
                 .when('is_available', { is: true, then: Joi.required() }),
             is_available: Joi.boolean().required(),
         })
