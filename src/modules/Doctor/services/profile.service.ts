8ee47383ecba5347e33432f5efa6602002f056ea
mahmoud <mamoidtamer300@gmail.com>
2026-06-14 05:05:36 +0300

    .

--- src/modules/Doctor/services/profile.service.ts
+++ src/modules/Doctor/services/profile.service.ts
@@ -209,12 +209,12 @@ export const editDoctorSchedule = async (doctor: User, schedule: { day_of_week: 
 
 };
 
-export const getAvailableDoctors = async (reqQuery: { search: string, status: string, sort: string, page: string, limit: string }) => {
+export const getAvailableDoctors = async (reqQuery: { search?: string, status?: string, sort?: string, page?: string, limit?: string }) => {
 
     const { search, status, sort } = reqQuery;
 
     const page = Number(reqQuery.page) || 1;
-    const limit = Number(reqQuery.limit) || 10;
+    const limit = Number(reqQuery.limit) > 5 ? 5 : Number(reqQuery.limit);
     const offset = (page - 1) * limit;
 
     const cacheKey = `doctorsAvailable:${page}_${limit}_${sort}_${status}_${search}`;
