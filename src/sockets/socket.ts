e65c9ee2a953ac660145085af960e351b4761e1c
mahmoud <mamoidtamer300@gmail.com>
2026-06-17 00:22:59 +0300

    refactoring

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -24,6 +24,7 @@ export const initSocket = (server: any) => {
 
     io.on("connection", (socket) => {
         console.log("Socket connected :", socket.id);
+        console.log(`Connected  user:${(socket as any).user.id}`);
         socket.join(`user:${(socket as any).user.id}`);
 
         chatSockets(io, socket);
